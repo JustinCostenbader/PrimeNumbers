27be27ed7244a01adb4bd64f0907b7ef959ca21b
Justin Costenbader <justincostenbader@gmail.com>
2021-09-13 21:59:31 -0400

    Attempt 1 adding GitIgnore

---
+++ Primes/Primes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Primes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool prime = false;
+            int j;
+            
+            for (int i = 2; i <= 1000; i++)
+            {
+                for (j = 2; j < i; j++)  
+                {  
+                    if (i % j == 0)  
+                    {  
+                        prime = true;  
+                        break;  
+                    }
+                }  
+                if (!prime)  
+                    Console.Write("{0} ", j);  
+                else  
+                    prime = false;
+            }
+            
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
